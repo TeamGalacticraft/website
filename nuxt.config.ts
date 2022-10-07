7248a7217cca7f168f83b9b7b156359de4258000
Alex Long <alex@feeditback.com>
2022-10-07 07:23:40 +0100

    changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,4 +10,21 @@ export default defineNuxtConfig({
       },
     },
   },
+  meta: {
+    title: "Galacticraft",
+    htmlAttrs: {
+      lang: "en-GB",
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { rel: "apple-touch-icon", href: "/apple-touch-icon.png" },
+      {
+        hid: "description",
+        name: "description",
+        content: "",
+      },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
 });
